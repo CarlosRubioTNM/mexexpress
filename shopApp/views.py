d71ba717373c78fd81c093a6819d1627c88aff0e
Carlos Rubio <carlos.rr@chihuahua2.tecnm.mx>
2025-02-22 13:38:13 -0600

    Clase 21 de febrero

--- shopApp/views.py
+++ shopApp/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola mundo desde Django!")
+    my_context = {
+        'message' : 'Largo de aquí!',
+        'products' : [
+            'Mascarilla hidratante de sábila.',
+            'Consola de videojuegos portátil XE150',
+            'Reloj de pulsera Gótico de Snoopy',
+            'Camisa para caballero de algodón',
+            'Peluche de Batman tamaño real',
+        ],
+    }
+    return render(request, 'shopApp/index.html', context=my_context)
+    #return HttpResponse("Hola mundo desde Django!")
