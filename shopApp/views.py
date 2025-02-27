06e1d684e4b2ef032f1439eff4593059e47a9ef7
Carlos Rubio <carlos.rr@chihuahua2.tecnm.mx>
2025-02-27 14:47:59 -0600

    Clase 27 febrero

--- shopApp/views.py
+++ shopApp/views.py
@@ -4,13 +4,29 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     my_context = {
+        'user' : 'Carlos',
         'message' : 'Largo de aquí!',
-        'products' : [
-            'Mascarilla hidratante de sábila.',
-            'Consola de videojuegos portátil XE150',
-            'Reloj de pulsera Gótico de Snoopy',
-            'Camisa para caballero de algodón',
-            'Peluche de Batman tamaño real',
+        'special_offers' : [
+            {
+                'name' : 'Mascarilla hidratante de sábila',
+                'cost' : 14.00
+            },
+            {
+                'name' : 'Consola de videojuegos portátil XE150',
+                'cost' : 450.00
+            },
+            {
+                'name' : 'Reloj de pulsera Gótico de Snoopy',
+                'cost' : 160.00
+            },
+            {
+                'name' : 'Camisa para caballero de algodón',
+                'cost' : 200.00
+            },
+            {
+                'name' : 'Peluche de Batman tamaño real',
+                'cost' : 15600.00
+            },
         ],
     }
     return render(request, 'shopApp/index.html', context=my_context)
