6b3f2881793f589c714a8f3507ace188557fe079
Carlos Rubio <carlos.rr@chihuahua2.tecnm.mx>
2025-03-24 14:51:49 -0600

    Clase 24 de marzo

--- mexexpress/settings.py
+++ mexexpress/settings.py
@@ -40,10 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shopApp'
+    'shopApp',
+    'corsheaders'
 ]
 
+#Cors Headers
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [STATIC_DIR,]
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
